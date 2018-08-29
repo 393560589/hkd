27f1b25145534b2ff7356388149cde4cbc60c4b2
393560589@qq.com <393560589@qq.com>
2018-08-29 15:31:33 +0800

    11111

--- hkdyy/src/pages/home/test.js
+++ hkdyy/src/pages/home/test.js
@@ -1,9 +1,10 @@
 import React,{PureComponent} from 'react'
-import { Button } from 'antd'
+import { Tooltip,Button } from 'antd'
 import { connect } from 'dva'
+import { LeftNav } from '../../components/test'
 @connect(({index})=>({index}))
 
-export default class Home extends PureComponent{
+export default class Test extends PureComponent{
   componentDidMount(){
     console.log(this.props)
   }//js挂载后,基本js交互行为可以写这里
@@ -13,11 +14,13 @@ export default class Home extends PureComponent{
   render(){
     return (
       <div>
-
+        <LeftNav>
+          这是一个测试
+        </LeftNav>
         <Button onClick={()=>this.getProps()} type={'primary'}> 按钮</Button>
+        <Button onClick={()=>this.getProps()} type={'primary'}> 按钮2</Button>
       </div>
 
     )
   }
 }
-
